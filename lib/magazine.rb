d898352874646b9fd757a6bd0c4c22c75b6ba5ab
Wangari Samantha <samanthanyams5@gmail.com>
2023-05-29 08:41:45 +0300

    magazine

---
+++ lib/magazine.rb
@@ -0,0 +1,48 @@
+
+ 
+
+require_relative "./article.rb"
+require_relative "./author.rb"
+
+class Magazine
+  attr_accessor :name, :category
+  @@all = []
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all << self
+  end
+
+  def contributors
+    Article.all.select do |article|
+      article.magazine == self
+    end.map do |article|
+      article.author
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name (name)
+    Magazine.all.find do |magazine|
+      magazine.name == name
+    end
+  end
+
+  def contributing_authors
+    self.contributors.select do|author|
+      author.articles.count > 2
+    end
+  end
+
+  def article_titles
+    Article.all.select do |article|
+      article.magazine == self
+    end.map do |article|
+      article.title
+    end
+  end
+
+end
