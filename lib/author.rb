30089e2c544f189282f90190d0d3523e3faaba56
Wangari Samantha <samanthanyams5@gmail.com>
2023-05-29 08:17:36 +0300

    author

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+require_relative "./article.rb"
+require_relative "./magazine.rb"
+
+class Author
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def magazines
+   self.articles.map do |article|
+      article.magazine
+    end.uniq
+  end
+
+  def topic_areas
+    self.articles.map do |article|
+      article.magazine.category
+    end
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+  end
+
+    def add_article(magazine,title)
+     Article.new(self,magazine,title)
+  end
+
+end
